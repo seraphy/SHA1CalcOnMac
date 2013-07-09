b52180f69decbf4d4d17079256bc187a6ceb1cfb
seraphy <seraphy@users.sourceforge.jp>
2013-07-10 01:07:58 +0900

    URLにイコール文字が含まれる場合に正しくデータを復元できなかった問題の修正

--- SHA1CalcOnMac/HashItem.m
+++ SHA1CalcOnMac/HashItem.m
@@ -77,48 +77,63 @@
 {
     NSArray *cols = [str componentsSeparatedByString: sep];
     NSInteger len = [cols count];
-    if (len > 1) {
-        if ([[cols objectAtIndex: 0] hasPrefix: @"HashItem:"]) {
-            // シグネチャ確認OK
-            HashItem *result = [[[HashItem alloc] init] autorelease];
-            for (NSInteger idx = 1; idx < len; idx++) {
-                NSString *col = [cols objectAtIndex: idx];
-                NSArray *tokens = [col componentsSeparatedByString: @"="];
-                if ([tokens count] == 2) {
-                    NSString *key = [tokens objectAtIndex: 0];
-                    NSString *val = [[tokens objectAtIndex: 1] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-                    
-                    if ([key isEqualToString: @"url"]) {
-                        [result setUrl: [NSURL URLWithString: val]];
-                        
-                    } else if ([key isEqualToString: @"checked"]) {
-                        [result setChecked: [val intValue] != 0];
+    if (len <= 1) {
+        NSLog(@"invalid-separator? :%@", str);
+        return nil;
+    }
 
-                    } else if ([key isEqualToString: @"fileSize"]) {
-                        [result setFileSize: [val longLongValue]];
-                        
-                    } else if ([key isEqualToString: @"sha1"]) {
-                        [result setSha1hash: val];
-                        
-                    } else if ([key isEqualToString: @"md5"]) {
-                        [result setMd5hash: val];
-                    }
-                }
-            }
+    if (![[cols objectAtIndex: 0] hasPrefix: @"HashItem:"]) {
+        // シグネチャ確認
+        NSLog(@"invalid-signature? :%@", str);
+        return nil;
+    }
+
+    HashItem *result = [[[HashItem alloc] init] autorelease];
+    for (NSInteger idx = 1; idx < len; idx++) {
+        NSString *column = [cols objectAtIndex: idx];
+        
+        NSScanner *scanner = [NSScanner scannerWithString: column];
+        NSString *key;
+        if (![scanner scanUpToString: @"=" intoString: &key]) {
+            NSLog(@"invalid-keyvalue-format: %@", str);
+            return nil;
+        }
+        key = [key stringByTrimmingCharactersInSet:
+               [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+
+        [scanner scanString: @"=" intoString: nil];
+        NSString *val = [[column substringFromIndex: [scanner scanLocation]] 
+                         stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        
+        if ([key isEqualToString: @"url"]) {
+            [result setUrl: [NSURL URLWithString: val]];
             
-            // ハッシュ値が計算済みであれば成功とみなす.(そうでなければ失敗とみなす)
-            if ([[result sha1hash] length] != 0) {
-                [result setState: hashItem_calced];
-            } else {
-                [result setState: hashItem_failed];
-            }
-
-            if ([result url]) {
-                return result;
-            }
+        } else if ([key isEqualToString: @"checked"]) {
+            [result setChecked: [val intValue] != 0];
+            
+        } else if ([key isEqualToString: @"fileSize"]) {
+            [result setFileSize: [val longLongValue]];
+            
+        } else if ([key isEqualToString: @"sha1"]) {
+            [result setSha1hash: val];
+            
+        } else if ([key isEqualToString: @"md5"]) {
+            [result setMd5hash: val];
         }
     }
-    NSLog(@"unknown :%@", str);
+    
+    // ハッシュ値が計算済みであれば成功とみなす.(そうでなければ失敗とみなす)
+    if ([[result sha1hash] length] != 0) {
+        [result setState: hashItem_calced];
+    } else {
+        [result setState: hashItem_failed];
+    }
+    
+    if ([result url]) {
+        return result;
+    }
+
+    NSLog(@"invalid-url? :%@", str);
     return nil;
 }
 
